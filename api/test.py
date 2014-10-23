90a8de9792b44f5fb36b132f6d2a15b58561becc
Bill Mill <bill.mill@gmail.com>
2014-10-23 08:52:14 -0400

    update test to handle booleans

--- api/test.py
+++ api/test.py
@@ -3,17 +3,25 @@ import os
 import os.path
 import re
 
-
+# Just a handy function for testing things in the interpreter. Pass it a function
+# and this function will pass each api json object to it
 #def walk(fn):
 #    for dir in (d for d in os.listdir('.') if len(d) == 3):
 #        for f in (f for f in os.listdir("./{}".format(dir)) if f.endswith("json")):
 #            fn(json.load(file("./{}/{}".format(dir, f))))
 
+def rem(lst, name):
+    try:
+        lst.remove(name)
+    except ValueError: pass
+
 def index(args, dirname, fnames):
     index, attrs = args
-    for f in fnames:
-        if f in ["filters", "institutions.json"]: continue
 
+    rem(fnames, "filters")
+    rem(fnames, "institutions.json")
+
+    for f in fnames:
         f = os.path.join(dirname, f)
         if f.endswith('.json') and os.path.isfile(f):
             try:
@@ -22,19 +30,28 @@ def index(args, dirname, fnames):
                     index.setdefault(attr, {}).setdefault(j[attr], {})[j["facility_code"]] = None
                     #typeindex.setdefault(j[attr], []).append(j["facility_code"])
             except:
-                print f
+                print "Exception reading file {}".format(f)
 
 def norm(string):
-    return re.sub("\W", "", string).lower()
+    if string is None:  string = "null"
+    if string is True:  string = "true"
+    if string is False: string = "false"
+
+    try:
+        return re.sub("\W", "", string).lower()
+    except TypeError:
+        print string
+        raise
 
 attrindexes = {}
-os.path.walk('.', index, (attrindexes, ("type", "state", "country")))
+os.path.walk('.', index, (attrindexes, ("type", "state", "country", "poe", "yr", "student_veteran", "eight_keys")))
 
 for attr in attrindexes:
     dirname = "filters/{}".format(attr)
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
     for typ, codes in attrindexes[attr].iteritems():
-        if typ is None:
-            typ = "null"
-        json.dump(codes, file("filters/{}/{}.json".format(attr, norm(typ)), 'w'))
+        fname = "filters/{}/{}.json".format(attr, norm(typ))
+        if typ in ("poe", "yr", "student_veteran", "eight_keys"):
+            codes["__attr__"] = typ
+        json.dump(codes, file(fname, 'w'))
