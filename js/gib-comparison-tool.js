16c57bcf0bba28d94bcc47380ff8d73c67bda818
Patrick Campbell <patrick.campbell@cfpb.gov>
2014-11-10 00:46:34 -0500

    Update gib-comparison-tool.js

--- js/gib-comparison-tool.js
+++ js/gib-comparison-tool.js
@@ -301,9 +301,9 @@ var GIBComparisonTool = (function () {
       $("#accreditation-row").hide();
     }else{
       $('#accreditation').text((institution.accredited ? 'Yes' : 'No') +  
-        "<p><a href='http://nces.ed.gov/collegenavigator/?id=" +
+        "<a href='http://nces.ed.gov/collegenavigator/?id=" +
         institution.cross +
-        "#accred' onclick=\"track('Tool Tips', 'School Summary / Link to Accreditors');\" target='newtab'>  See Accreditors &raquo;</a></p>");
+        "#accred' onclick=\"track('Tool Tips', 'School Summary / Link to Accreditors');\" target='newtab'>  See Accreditors &raquo;</a>");
       if(institution.accreditation_type) {
         $('#accreditation-type').text(institution.accreditation_type);
       }else{ $('#accreditation-type-row').hide(); }
