4ae806143f978a2718e04e62d7d7b68dc2163a43
Bill Mill <bill.mill@gmail.com>
2014-11-10 10:04:45 -0500

    make link a link instead of text
    fixes #82392654

--- js/gib-comparison-tool.js
+++ js/gib-comparison-tool.js
@@ -300,7 +300,7 @@ var GIBComparisonTool = (function () {
     if(institution.accredited == null) { 
       $("#accreditation-row").hide();
     }else{
-      $('#accreditation').text((institution.accredited ? 'Yes' : 'No') +  
+      $('#accreditation').html((institution.accredited ? 'Yes' : 'No') +  
         "<a href='http://nces.ed.gov/collegenavigator/?id=" +
         institution.cross +
         "#accred' onclick=\"track('Tool Tips', 'School Summary / Link to Accreditors');\" target='newtab'>  See Accreditors &raquo;</a>");
