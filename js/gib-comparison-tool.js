82d291e9aa39d22a17849316f2dcda7303319494
Patrick Campbell <patrick.campbell@cfpb.gov>
2015-07-21 13:16:42 -0400

    Updating HCM Logic (new variables)

--- js/gib-comparison-tool.js
+++ js/gib-comparison-tool.js
@@ -330,12 +330,12 @@ var GIBComparisonTool = (function () {
   };
 
   var getCautionIndicator = function() {
-    if(institution.accredited == null) { 
+    if(institution.hcm_status == null) { 
       $('#caution-indicator').html("None");
-    } else if(institution.accredited) {
-    	$('#caution-indicator').html("<a href='https://studentaid.ed.gov/sa/about/data-center/school/hcm' onclick='track('Tool Tips', 'School Summary / Caution Flag');' target='_blank' alt='Click here for more information.' title='To see more information about caution flags, please visit ED’s heightened cash monitoring resource page.'>Heightened Cash Monitoring List</a>" +
+    } else if(institution.hcm_status) {
+    	$('#caution-indicator').html("<a href='https://studentaid.ed.gov/sa/about/data-center/school/hcm' onclick='track('Tool Tips', 'School Summary / Caution Flag');' target='_blank' alt='Click here for more information.' title='To see more information about caution flags, please visit ED’s heightened cash monitoring resource page.'>Heightened Cash Monitoring</a>" +
 		"&nbsp; ("+
-	    	institution.accreditation_type +
+	    	institution.hcm_reason +
 	    	")");
     }
   };
